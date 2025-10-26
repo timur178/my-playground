74f6720dcc81342e38524d444983dc9fee31a037
timur <8tyronee@proton.me>
2025-10-26 00:58:02 -0500

    Add kubernetes to terraform providers

--- kafka-infra/providers.tf
+++ kafka-infra/providers.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 6.0" }
+    aws        = { source = "hashicorp/aws", version = "~> 6.0" }
+    kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.29.0" }
   }
 
   backend "s3" {}
@@ -15,4 +16,18 @@ provider "aws" {
       Environment = var.environment
     }
   }
+}
+
+# discover cluster endpoint/CA and token
+data "aws_eks_cluster" "this" {
+  name = var.eks_cluster_name
+}
+data "aws_eks_cluster_auth" "this" {
+  name = var.eks_cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
 }
