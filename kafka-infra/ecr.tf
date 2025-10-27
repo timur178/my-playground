95039620b3779470dfcd0d9d482ff0eb7449f2a4
timur <8tyronee@proton.me>
2025-10-26 22:11:28 -0500

    Change ecr to mutable

--- kafka-infra/ecr.tf
+++ kafka-infra/ecr.tf
@@ -13,7 +13,7 @@ locals {
 resource "aws_ecr_repository" "repos" {
   for_each             = local.repo_map
   name                 = each.value
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
   force_delete         = true
   image_scanning_configuration { scan_on_push = true }
   encryption_configuration { encryption_type = "AES256" }
